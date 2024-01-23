18b30286bd767ac78f11d5ecf3030cdae1ea1a12
David Geleijnse <113440@emmauscollege.nl>
2024-01-23 18:44:01 +0000

    hallo

--- script.js
+++ script.js
@@ -1,62 +1,52 @@
-/* Game opdracht
-   Informatica - Emmauscollege Rotterdam
-   Template voor een game in JavaScript met de p5 library
-
-   Begin met dit template voor je game opdracht,
-   voeg er je eigen code aan toe.
- */
-
-/*
- * instellingen om foutcontrole van je code beter te maken 
- */
-///<reference path="p5.global-mode.d.ts" />
+<reference path="p5.global-mode.d.ts" />
 "use strict"
 
-/* ********************************************* */
-/* globale variabelen die je gebruikt in je game */
-/* ********************************************* */
 const SPELEN = 1;
 const GAMEOVER = 2;
+
+/*const A = 65;
+const D = 68
+const SPACE = 32;
+*/
+
 var spelStatus = SPELEN;
 
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
 
-/* ********************************************* */
-/* functies die je gebruikt in je game           */
-/* ********************************************* */
 
-/**
- * Updatet globale variabelen met posities van speler, vijanden en kogels
- */
 var beweegAlles = function() {
   // speler
+  /*if(keydown(A))
+    spelerX = spelerX +1;
+  }
+  
+  if(keydown(D)) {
+    spelerX = spelerX -1;
+  }
+ 
+  if(keydown(SPACE)) {
+    spelerY = spelerY +1;
+  }*/
 
   // vijand
 
   // kogel
+
 };
 
-/**
- * Checkt botsingen
- * Verwijdert neergeschoten dingen
- * Updatet globale variabelen punten en health
- */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
 
   // botsing kogel tegen vijand
 
   // update punten en health
-
 };
 
-/**
- * Tekent spelscherm
- */
 var tekenAlles = function() {
   // achtergrond
-
+  //fill("blue");
+  //rect(0, 0, 1270, 720);
   // vijand
 
   // kogel
@@ -71,37 +61,18 @@ var tekenAlles = function() {
 
 };
 
-/**
- * return true als het gameover is
- * anders return false
- */
 var checkGameOver = function() {
   // check of HP 0 is , of tijd op is, of ...
   return false;
 };
 
-/* ********************************************* */
-/* setup() en draw() functies / hoofdprogramma   */
-/* ********************************************* */
-
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
 function setup() {
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+
   createCanvas(1280, 720);
 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
   background('blue');
 }
 
-/**
- * draw
- * de code in deze functie wordt 50 keer per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
 function draw() {
   if (spelStatus === SPELEN) {
     beweegAlles();
@@ -114,5 +85,5 @@ function draw() {
   if (spelStatus === GAMEOVER) {
     // teken game-over scherm
 
+    }
   }
-}
